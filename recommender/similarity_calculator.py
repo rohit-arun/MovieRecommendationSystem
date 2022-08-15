61b359a61434d9bb5de9fc62dde54e89cfd57430
Rohit Arun <65969016+rohit-arun@users.noreply.github.com>
2022-08-15 15:49:05 +0530

    Update similarity_calculator.py

---
+++ recommender/similarity_calculator.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity 
+
+export_movies = pd.read_pickle('./pkl/export_movies.pkl')
+movies_tags = pd.read_pickle('./pkl/movies_tags.pkl')
+
+vectorizer = CountVectorizer()
+feature_vectors = vectorizer.fit_transform(movies_tags['tags'])
+similarity = cosine_similarity(feature_vectors)
